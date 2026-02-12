bb0d9ea51033f18c1531f29f68888f7e2280b4df
Haruo <me@haruomaki.jp>
2026-02-12 20:52:49 +0900

    取得した問題をレンダリングする

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -4,29 +4,57 @@ import { useEffect, useState } from 'react';
 
 type Question = {
     word: string;
-    choices: [string];
+    choices: string[];
     correct: number;
 };
 
 export function Quiz() {
     const [questions, setQuestions] = useState<Question[]>([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         api(`/questions?level=5&k=3`)
             .then(res => res.json())
             .then((data: Question[]) => {
+                console.debug(data);
                 setQuestions(data);
             });
-    }, []); // ← 空にする
+    }, []);
 
+    // 問題の取得に手こずっている場合
+    if (questions.length === 0) {
+        return <p>Loading...</p>;
+    }
+
+    const currentQ = questions[currentIndex];
     return (
         <div>
-            <p>こんにちは</p>
+            <h3>
+                問題 {currentIndex + 1} / {questions.length}
+            </h3>
 
-            <pre style={{ textAlign: "left" }}>{JSON.stringify(questions, null, 2)}</pre>
+            <h2>{currentQ.word}</h2>
+
+            <ul style={{ listStyle: "none", padding: 0 }}>
+                {currentQ.choices.map((choice, i) => (
+                    <li key={i}>
+                        <button style={{ margin: "8px 0" }}>
+                            {choice}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            <button
+                onClick={() => {
+                    setCurrentIndex((currentIndex + 1) % questions.length);
+                }}
+            >
+                次へ
+            </button>
 
             <Link to="/">
-                <button>戻る</button>
+                <button className="back-button">戻る</button>
             </Link>
         </div>
     );
